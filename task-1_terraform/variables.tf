e1bda18da51f0d02e6a9e2ce222623d635a1c1a6
adigaur20 <94065000+adigaur20@users.noreply.github.com>
2021-11-11 16:56:08 +0530

    Update variables.tf

--- task-1_terraform/variables.tf
+++ task-1_terraform/variables.tf
@@ -6,17 +6,17 @@ variable "tenant" { type="string" }
 
 variable "vpc_cidr" { type="string" }
 variable "ami_id" { type="string" }
-variable "dmz_networks" { type="list(string)" }
-variable "app_networks" { type="list(string)" }
-variable "data_networks" { type="list(string)" }
-variable "dmz_instances" { type="list(string)" }
-variable "app_instances" { type="list(string)" }
-variable "dmz_instance_type" { type="list(string)" }
-variable "app_instance_type" { type="list(string)t" }
+variable "dmz_networks" { type="list" }
+variable "app_networks" { type="list" }
+variable "data_networks" { type="list" }
+variable "dmz_instances" { type="list" }
+variable "app_instances" { type="list" }
+variable "dmz_instance_type" { type="list" }
+variable "app_instance_type" { type="list" }
 variable "az" { type="list(string)" default=["a","b","c"] }
 variable "pubkey" { type="string" }
 variable "app_fqdn" { type="string" }
-variable "ssh_allowed" { type="list(string)" }
+variable "ssh_allowed" { type="list" }
 variable "certsValid" { type="string" default=false }
 variable "hosted_zone_id" { type="string" }
 
