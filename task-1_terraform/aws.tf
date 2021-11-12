bc28f14bb6b92e7e273f4e55a8f5dee4aebada98
adigaur20 <94065000+adigaur20@users.noreply.github.com>
2021-11-12 13:13:42 +0530

    Update aws.tf

--- task-1_terraform/aws.tf
+++ task-1_terraform/aws.tf
@@ -1,15 +1,16 @@
 ######## This file is for provider aws
 provider "aws" {
-  #version = "~> 1.24.0"
-  version = "~> 2.5.0"
+  version = "~> 3.65.0"
   region     = "${var.region}"
 }
-terraform {
-  backend "s3" {
-    bucket = "demo-tf-state"
-    dynamodb_table = "demo-tf-state"
-    key    = "demo/tfstate"
-    region = "eu-central-1"
-    role_arn     = "arn:aws:iam::<account_number>:role/Terraform"
-  }
-}
+
+### Best prcatice is to use backend but can't use it here in demo
+#terraform {
+#  backend "s3" {
+#    bucket = "demo-tf-state"
+#    dynamodb_table = "demo-tf-state"
+#    key    = "demo/tfstate"
+#    region = "eu-central-1"
+#    role_arn     = "arn:aws:iam::<account_number>:role/Terraform"
+#  }
+#}
