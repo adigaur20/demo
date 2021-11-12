ab04b7725cb8fceba18229f45b78e31024a49bed
adigaur20 <94065000+adigaur20@users.noreply.github.com>
2021-11-12 13:24:54 +0530

    Update network.tf

--- task-1_terraform/network.tf
+++ task-1_terraform/network.tf
@@ -2,10 +2,7 @@
 resource "aws_vpc" "default" {
   cidr_block = "${var.vpc_cidr}"
   enable_dns_hostnames = true
-  tags {
-    VPC = "${var.tenant}"
-    Name = "${var.tenant}"
-  }
+  
 }
 
 #Create subnets
