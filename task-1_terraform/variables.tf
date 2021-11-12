78da3206699d2052b6daeb5320cabf0821123c0f
adigaur20 <94065000+adigaur20@users.noreply.github.com>
2021-11-12 13:04:14 +0530

    Update variables.tf

--- task-1_terraform/variables.tf
+++ task-1_terraform/variables.tf
@@ -1,21 +1,21 @@
 #
 # define Variables
 #
-variable "region" { type="string" }
-variable "tenant" { type="string" }
+variable "region" { type=string }
+variable "tenant" { type=string }
 
-variable "vpc_cidr" { type="string" }
-variable "ami_id" { type="string" }
-variable "dmz_networks" { type="list" }
-variable "app_networks" { type="list" }
-variable "data_networks" { type="list" }
-variable "dmz_instances" { type="list" }
-variable "app_instances" { type="list" }
-variable "dmz_instance_type" { type="list" }
-variable "app_instance_type" { type="list" }
-variable "az" { type="list(string)" default=["a","b","c"] }
-variable "pubkey" { type="string" }
-variable "ssh_allowed" { type="list" }
-variable "certsValid" { type="string" default=false }
-variable "hosted_zone_id" { type="string" }
+variable "vpc_cidr" { type=string }
+variable "ami_id" { type=string }
+variable "dmz_networks" { type=list(string) }
+variable "app_networks" { type=list(string) }
+variable "data_networks" { type=list(string) }
+variable "dmz_instances" { type=list(string) }
+variable "app_instances" { type=list(string) }
+variable "dmz_instance_type" { type=list(string) }
+variable "app_instance_type" { type=list(string) }
+variable "az" { type=list(string) default=["a","b","c"] }
+variable "pubkey" { type=string }
+variable "ssh_allowed" { type=list(string) }
+variable "certsValid" { type=string default=false }
+variable "hosted_zone_id" { type=string }
 
