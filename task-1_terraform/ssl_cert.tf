2e6c393699208e6493c09d3e059766f99686a3d3
adigaur20 <94065000+adigaur20@users.noreply.github.com>
2021-11-12 13:21:58 +0530

    Update ssl_cert.tf

--- task-1_terraform/ssl_cert.tf
+++ task-1_terraform/ssl_cert.tf
@@ -1,6 +1,6 @@
 ### TO create SSl certificate for Application LB 
 
-resource "aws_acm_certificate" "sc" {
+resource "aws_acm_certificate" "app" {
   domain_name = "${var.app_fqdn}"
   validation_method = "DNS"
   tags {
